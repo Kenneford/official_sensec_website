d7cba9518e648eac0bc17cd9eb99854f75acfb6d
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2024-11-29 19:10:36 -0100

    vite.config : base modified to handle both local and production development.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === "production" ? "/official_sensec_website/" : "/",
   optimizeDeps: {
     include: [
       "@mui/x-date-pickers",
@@ -11,5 +12,4 @@ export default defineConfig({
       "date-fns",
     ],
   },
-  base: "/official_sensec_website/",
-});
+}));
