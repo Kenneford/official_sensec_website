a4ced019a0b59f94c15f70380a866b63139886fc
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2024-11-30 20:06:14 -0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "production" ? "/official_sensec_website/" : "/",
+  // base: mode === "production" ? "/official_sensec_website/" : "/",
   build: {
     rollupOptions: {
       external: ["@mui/x-date-pickers"],
