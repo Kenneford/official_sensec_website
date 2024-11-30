5a348ca51cb56283152d9be06a1cf9a64c0f4228
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2024-11-30 22:59:57 -0100

    Update api.js

--- src/apiEndPoint/api.js
+++ src/apiEndPoint/api.js
@@ -1,7 +1,7 @@
 export const API_ENDPOINT = "http://localhost:7005/api";
 // export const SENSEC_API_ENDPOINT =
 //   "http://192.168.22.157:7006/api/sensec_db/v1";
-export const SENSEC_API_ENDPOINT = "https://sensec-database.onrender.com/";
+export const SENSEC_API_ENDPOINT = "https://sensec-database.onrender.com";
 // export const SENSEC_API_ENDPOINT =
 //   "http://192.168.178.22:7006/api/sensec_db/v1";
 export const SENSOSA_API_ENDPOINT =
