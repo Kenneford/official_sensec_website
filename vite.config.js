1bc1d0f751129788db41f8fcdecb1b1487dc21dc
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2024-11-30 19:49:12 -0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === "production" ? "/official_sensec_website/" : "/",
-  optimizeDeps: {
-    include: [
-      "@mui/x-date-pickers",
-      "@mui/x-date-pickers/AdapterDateFns",
-      "date-fns",
-    ],
-  },
-  resolve: {
-    alias: {
-      "date-fns/_lib/format/longFormatters":
-        "date-fns/esm/_lib/format/longFormatters/index.js",
-    },
-  },
 }));
