3e49973c14525c6640da8ed7fd504be01c4aa61b
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2024-12-01 00:07:21 -0100

    404.html and vite.config modified.

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Make sure to set base URL if using a subdirectory
   build: {
+    outDir: "dist", // Default output directory
     rollupOptions: {
+      // Ensuring all routes fallback to index.html
+      input: "/index.html",
       external: ["@mui/x-date-pickers"],
     },
+  },
+  server: {
+    // If you need to enable history fallback for SPA routing during development
+    historyApiFallback: true,
   },
 });
