95bca31e2c479d899cb7b4478668559467e38302
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2024-11-30 23:26:34 -0100

    Revert "Update vite.config.js"
    This reverts commit bdbf49e1a0c58ed7d42d3b102830c05d6dfb1b52.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "production" ? "/official_sensec_website" : "/",
+  base: mode === "production" ? "/sensec" : "/",
   build: {
     rollupOptions: {
       external: ["@mui/x-date-pickers"],
