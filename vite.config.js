b7b45c87045882d62e3ee750c5482a6d88e71104
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2025-02-22 02:16:53 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import ViteSitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // const isProduction = mode === "production";
-
   return {
-    plugins: [
-      react(),
-      // ViteSitemap({
-      //   siteUrl: isProduction
-      //     ? "https://senyashs.com"
-      //     : "http://192.168.178.22:2025/",
-      //   changefreq: "daily",
-      //   priority: 0.7,
-      // }),
-    ],
+    plugins: [react()],
     base: "/",
     optimizeDeps: {
       include: ["@mui/x-date-pickers", "date-fns", "@date-io/date-fns"],
